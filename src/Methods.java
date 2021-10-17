517c0eedd8ca96a47c9c6d20dbde90ea62aed34d
Christian Palombo <palombochristian21@gmail.com>
2021-10-16 22:14:44 -0400

    First Commit

---
+++ src/Methods.java
@@ -0,0 +1,13 @@
+public class Methods {
+
+
+    public static void main(String[] args) {
+        System.out.println(method(12,24,48));
+    }
+
+    public static double method(double value1, double value2, double value3)
+    {
+        return (value1 / value2) * value3;
+    }
+
+}
